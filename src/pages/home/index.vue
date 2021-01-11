83ec062f58d48f19054f6196af5621340a3ef691
yangshuo <yangshuo@teamsun.com.cn>
2021-01-11 16:14:22 +0800

    基本骨架

---
+++ src/pages/home/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <van-button type="default" @click="nextStep">开始制作</van-button>
+  </div>
+</template>
+
+<script>
+import { Button } from "vant";
+import router from '../../../router/routes'
+
+export default {
+  name: "home",
+  components: {
+    vanButton: Button,
+  },
+  props: {
+    msg: String,
+  },
+  mounted() {
+
+  },
+  methods: {
+      nextStep() {
+          console.log('xiayibu');
+          router.push('/makePhoto')
+      }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
