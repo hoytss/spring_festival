ecc8fa991db0974bfe25395238ccb9b0182ee951
yangshuo <yangshuo@teamsun.com.cn>
2021-01-25 01:03:43 +0800

    先提交这些，看看真机效果，睡觉ZZzzz，困

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,46 +3,76 @@
 		<!-- <van-button type="default" @click="nextStep">开始制作</van-button> -->
 		<!-- <van-image :src=bg class="temp" @click="nextStep"/> -->
 		<div class="door-wrap">
-			<div class="door-left" @click="openTheDoor('left')">
-				举头望明月
-				<div class="open-left">
-					<div class="img" :style="openDoorLeft">
-						<van-image :src="doorLeft" class="temp1" />
+			<div class="title">
+				<van-image :src="title" />
+			</div>
+      <div class="horizontal-batch">
+        <van-image :src="Light" />
+      </div>
+			<div class="door-open">
+				<div class="door-left" @click="openTheDoor('left')">
+					<div class="couplet-left">
+						<van-image :src="coupletLeft" />
+					</div>
+					<div class="open-left">
+						<div class="img" :style="openDoorLeft">
+							<van-image :src="doorLeft" class="temp1" />
+						</div>
+					</div>
+				</div>
+				<div class="door-right" @click="openTheDoor('right')">
+					<div class="couplet-right">
+						<van-image :src="coupletRight" />
+					</div>
+					<div class="open-right">
+						<div class="img" :style="openDoorRight">
+							<van-image :src="doorRight" class="temp1" />
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="door-right" @click="openTheDoor('right')">
-				低头思故乡
-				<div class="open-right">
-					<div class="img" :style="openDoorRight">
-						<van-image :src="doorRight" class="temp1" />
-					</div>
-				</div>
-			</div>
+      <div class="door-firecracker">
+        <van-image :src="firecracker" />
+      </div>
+      <div class="door-cloud">
+        <van-image :src="cloud" />
+      </div>
 		</div>
 	</div>
 </template>
 
 <script>
-import { Button, Image } from "vant";
+import { Button, Image, Dialog } from "vant";
 import router from "../../../router/routes";
-import background from "../../assets/images/home_img.jpg";
-import doorLeftImg from "../../assets/images/doorleft.jpg";
-import doorRightImg from "../../assets/images/door.jpg";
+import doorLeftImg from "../../assets/images/home/door_right.png";
+import doorRightImg from "../../assets/images/home/door_left.png";
+import LightImg from "../../assets/images/home/light.png";
+import Title from "../../assets/images/home/title.png";
+import Cloud from "../../assets/images/home/cloud.png";
+import Firecracker from "../../assets/images/home/firecracker.png";
+import coupletLeftImg from "../../assets/images/home/nianyefan.png";
+import coupletRightImg from "../../assets/images/home/quanjiafu.png";
 
 export default {
 	name: "home",
 	components: {
 		[Button.name]: Button,
 		[Image.name]: Image,
+		[Dialog.Component.name]: Dialog.Component,
 	},
 	data() {
 		return {
-			bg: background,
 			doorLeft: doorLeftImg,
-      doorRight: doorRightImg,
-      openDoorLeft: {},
-      openDoorRight: {}
+			doorRight: doorRightImg,
+      Light: LightImg,
+      title: Title,
+      cloud: Cloud,
+      firecracker: Firecracker,
+			coupletLeft: coupletLeftImg,
+			coupletRight: coupletRightImg,
+			openDoorLeft: {},
+			openDoorRight: {},
+			homeNum: null,
 		};
 	},
 	props: {
@@ -52,81 +82,127 @@ export default {
 	methods: {
 		nextStep() {
 			router.push("/makePhoto");
-    },
-    // 选择类型+开门
-    openTheDoor(arg) {
-      console.log(arg);
-      this.openDoorLeft = {
-        opacity: 0,
-        '*filter': 'alpha(opacity=0)',
-        filter: 'progid:DXImageTransform.Microsoft.Alpha(Opacity=0)',
-        transform: 'perspective(600px) rotateY(90deg)',
-        '-webkit-transform': 'perspective(600px) rotateY(90deg)',
-        '-moz-transform': 'perspective(600px) rotateY(90deg)'
-      }
-      this.openDoorRight = {
-					opacity: 0,
-					'*filter': 'alpha(opacity=0)',
-					filter: 'progid:DXImageTransform.Microsoft.Alpha(Opacity=0)',
-					transform: 'perspective(600px) rotateY(-90deg)',
-					'-webkit-transform': 'perspective(600px) rotateY(-90deg)',
-					'-moz-transform': 'perspective(600px) rotateY(-90deg)'
-      }
-      // 根据arg参数判断条件跳转
+		},
+		// 选择类型+开门
+		openTheDoor(arg) {
+			console.log(arg);
+			Dialog.alert({
+				message: "请先选择家庭人数哦～",
+			}).then(() => {
+				// on close
+			});
+			this.openDoorLeft = {
+				opacity: 0,
+				"*filter": "alpha(opacity=0)",
+				filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)",
+				transform: "perspective(600px) rotateY(90deg)",
+				"-webkit-transform": "perspective(600px) rotateY(90deg)",
+				"-moz-transform": "perspective(600px) rotateY(90deg)",
+			};
+			this.openDoorRight = {
+				opacity: 0,
+				"*filter": "alpha(opacity=0)",
+				filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)",
+				transform: "perspective(600px) rotateY(-90deg)",
+				"-webkit-transform": "perspective(600px) rotateY(-90deg)",
+				"-moz-transform": "perspective(600px) rotateY(-90deg)",
+			};
+			// 根据arg参数判断条件跳转
 
-      // 开完门之后进入下一页
-      setTimeout(() => {
-        this.nextStep()
-      }, 2000);
-
-    }
+			// 开完门之后进入下一页
+			setTimeout(() => {
+				this.nextStep();
+			}, 2000);
+		},
 	},
 };
 </script>
 
 <style lang="less" scoped>
 .home {
+	height: 100%;
 	.temp {
 		width: 80%;
 		margin: 0 auto;
 	}
 	.door-wrap {
-		display: flex;
-		justify-content: center;
-		.door-left {
-			.open-left {
-				.img {
-					transition: all 3s;
-					-moz-transition: all 3s;
-					-webkit-transition: all 3s;
-					position: relative;
-					bottom: 0;
-					transform-origin: left;
-					-moz-transform-origin: left;
-					-webkit-transform-origin: left;
-					transform: perspective(600px) rotateY(0deg);
-					-webkit-transform: perspective(600px) rotateY(0deg);
-					-moz-transform: perspective(600px) rotateY(0deg);
+		height: 100%;
+		background-image: url("~@/assets/images/home/base_bg.png");
+		background-repeat: no-repeat;
+    background-size: 100% 100%; /* 背景图片宽度为容器宽度的100%，高度为容器高度的100% */
+		.horizontal-batch {
+			position: absolute;
+			top: 20%;
+    }
+    .title {
+			position: absolute;
+			top: 11%;
+		}
+		.door-open {
+			display: flex;
+			justify-content: center;
+			width: 81%;
+			margin: 0 auto;
+			padding-top: 76%;
+			.door-left {
+				position: relative;
+				.couplet-left {
+					position: absolute;
+					width: 68px;
+					z-index: 18;
+					left: 22px;
+					top: 28px;
+				}
+				.open-left {
+					.img {
+						transition: all 3s;
+						-moz-transition: all 3s;
+						-webkit-transition: all 3s;
+						position: relative;
+						bottom: 0;
+						transform-origin: left;
+						-moz-transform-origin: left;
+						-webkit-transform-origin: left;
+						transform: perspective(600px) rotateY(0deg);
+						-webkit-transform: perspective(600px) rotateY(0deg);
+						-moz-transform: perspective(600px) rotateY(0deg);
+					}
 				}
 			}
-		}
-	}
-	.door-right {
-		.open-right {
-			.img {
-				transition: all 3s;
-				-moz-transition: all 3s;
-				-webkit-transition: all 3s;
+			.door-right {
 				position: relative;
-				bottom: 0;
-				transform-origin: right;
-				-moz-transform-origin: right;
-				-webkit-transform-origin: right;
-				transform: perspective(600px) rotateY(0deg);
-				-webkit-transform: perspective(600px) rotateY(0deg);
-				-moz-transform: perspective(600px) rotateY(0deg);
+				.couplet-right {
+					position: absolute;
+					width: 68px;
+					z-index: 18;
+					right: 22px;
+					top: 28px;
+				}
+				.open-right {
+					.img {
+						transition: all 3s;
+						-moz-transition: all 3s;
+						-webkit-transition: all 3s;
+						position: relative;
+						bottom: 0;
+						transform-origin: right;
+						-moz-transform-origin: right;
+						-webkit-transform-origin: right;
+						transform: perspective(600px) rotateY(0deg);
+						-webkit-transform: perspective(600px) rotateY(0deg);
+						-moz-transform: perspective(600px) rotateY(0deg);
+					}
+				}
 			}
-		}
+    }
+    .door-cloud {
+      position: absolute;
+      bottom: -6px;
+    }
+    .door-firecracker {
+      position: absolute;
+      top: 231px;
+    }
 	}
 }
 </style>
